90d08dd687ce92dcb7e97f9536ddbf518be25f01
Dan <dan@djoc3.com>
2020-01-02 04:24:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  let nav = document.getElementById("nav-links");
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+}
